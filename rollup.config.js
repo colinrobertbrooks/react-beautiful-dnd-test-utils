aa67beb38b39171a42fc6f12ceafb1bb3d5e70cf
Colin Brooks <colinrobertbrooks@gmail.com>
2021-10-22 12:26:56 -0400

    add es build

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/index.cjs.js',
-    format: 'cjs'
-  },
+
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs'
+    },
+    {
+      file: 'dist/index.es.js',
+      format: 'es'
+    }
+  ],
   external: ['jest', '@testing-library/jest-dom', '@testing-library/react']
 };
